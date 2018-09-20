7991e7cad1ae43b585a2649f73bc8af02523d7a2
franciscoSevilla <43402325+franciscoSevilla@users.noreply.github.com>
2018-09-20 11:19:43 +0200

    Add files via upload
    Session 2 - Java Training

---
+++ HelloWordMavenDockerized/src/main/java/com/privalia/business/ICalculator.java
@@ -0,0 +1,13 @@
+package com.privalia.business;
+
+public interface ICalculator {
+	/* All interface methods are public by default*/
+	public int sum(int num1, int num2);
+	
+	public int substract(int num1, int num2);
+	
+	public int multiply (int num1, int num2);
+	
+	public int divide (int num1, int num2);
+
+}
---
+++ HelloWordMavenDockerized/src/test/java/com/privalia/business/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.privalia.business;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
