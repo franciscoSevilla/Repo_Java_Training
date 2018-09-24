fd074203157704a099d540d23cf30fc190771fb1
franciscoSevilla <43402325+franciscoSevilla@users.noreply.github.com>
2018-09-24 10:48:49 +0200

    New Project Java with Gradle

---
+++ HelloWorldGradleDockerized/src/main/java/com/privalia/model/Student.java
@@ -0,0 +1,140 @@
+package com.privalia.model;
+
+public class Student {
+	private int idStudent;
+	private String name;
+	private String surname;
+	private int age;
+
+	/**
+	 * @return the idStudent
+	 */
+	public int getIdStudent() {
+		return idStudent;
+	}
+
+	/**
+	 * @param idStudent the idStudent to set
+	 */
+	public void setIdStudent(int idStudent) {
+		this.idStudent = idStudent;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Student(int idStudent, String name, String surname, int age) {
+		super();
+		this.idStudent = idStudent;
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+	}
+
+	public Student() {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Student [idStudent=");
+		builder.append(idStudent);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", surname=");
+		builder.append(surname);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + idStudent;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (age != other.age)
+			return false;
+		if (idStudent != other.idStudent)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+	
+	
+
+}
